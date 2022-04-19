e52968c3d3268802aa806e93481fc8eb856564df
Mustapha-Nawawi-T <85850362+Mustapha-Nawawi-T@users.noreply.github.com>
2022-04-19 17:44:21 +0000

    progress

--- Vector/vector.hpp
+++ Vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mannouao <mannouao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 18:01:29 by mannouao          #+#    #+#             */
-/*   Updated: 2022/04/18 21:17:39 by mannouao         ###   ########.fr       */
+/*   Updated: 2022/04/19 17:43:56 by mannouao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ class vector
 		pointer			_begin;
 		size_type		_size;
 		size_type		_capacity;
+		allocator_type	_alloc;
 	
 	// default constructer
 	vector(const allocator_type& alloc = allocator_type())
@@ -52,19 +53,75 @@ class vector
 	
 	// fill constructer
 	vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
-		: _begin(alloc.allocate(n)),
+		: _begin(nullptr),
 		  _size(n),
 		  _capacity(n)
 	{
-		alloc.construct(n, val);
+		if (n > 0)
+		{
+			_begin = alloc.allocate(n);
+			pointer tmp = _begin;
+			for(int i = 0; i < n; i++)
+				alloc.construct(tmp++, val);
+		}
 	}
 
 	// range constructer
 	template <class Iterator>
-	vector(Iterator firts, Iterator last, const allocator_type& alloc = allocator_type())
+	vector(Iterator first, Iterator last, const allocator_type& alloc = allocator_type())
+		: _begin(nullptr),
+		  _size(0),
+		  _capacity(0)
 	{
-		
+		typename Iterator::difference_type n = first - last;
+		if (n > 0)
+		{
+			this->_begin = alloc.allocate(n);
+			pointer tmp = _begin;
+			for(; first != last; first++)
+			 	alloc.construct(tmp++, *first);
+		}
 	}
+
+	// copy constructor
+	vector(const vector& other)
+		: _begin(nullptr),
+		  _size(other._size),
+		  _capacity(other._size),
+		  _alloc(other._alloc)
+	{
+		if (other._size > 0)
+		{
+			_begin = _alloc.allocate(other._size);
+			pointer tmp = _begin;
+			for (int i = 0; i < other.size; i++)
+				_alloc.construct(tmp++, other[i]);
+		}
+	}
+
+	// begin()
+	iterator begin() { return (iterator(_begin)); }
+
+	//  const begin()
+	const_iterator begine() const { return (const_iterator(_begin)); }
+
+	// end()
+	iterator end() { return (iterator(_begin + _size)); }
+
+	// const end()
+	const_iterator end() { return (const_iterator(_begin + _size)); }
+
+	// begin()
+	reverse_iterator rbegin() { return (reverse_iterator(_begin)); }
+
+	//  const begin()
+	const_reverse_iterator rbegine() const { return (const_reverse_iterator(_begin)); }
+
+	// end()
+	reverse_iterator rend() { return (reverse_iterator(_begin + _size)); }
+
+	// const end()
+	const_reverse_iterator rend() { return (const_reverse_iterator(_begin + _size)); }
 };
 
 } // ft

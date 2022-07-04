a756e72e34b161b830d34465c23b76e5fb5afe15
Mustapha-Nawawi-T <85850362+Mustapha-Nawawi-T@users.noreply.github.com>
2022-07-04 18:29:26 +0100

    ++

--- iterator.hpp
+++ iterator.hpp
@@ -1,27 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   iterator.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mannouao <mannouao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/01 17:39:06 by mannouao          #+#    #+#             */
-/*   Updated: 2022/07/04 18:23:42 by mannouao         ###   ########.fr       */
+/*   Created: 2022/07/04 18:25:49 by mannouao          #+#    #+#             */
+/*   Updated: 2022/07/04 18:28:44 by mannouao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+# ifndef ITERATOR_HPP
+# define ITERATOR_HPP
 
-#include <type_traits>
-#include "type_traits.h"
-
-#include <algorithm>
-#include "algorithm.h"
-
-#include <vector>
-
-int main()
+namespace ft
 {
-	std::vector<int> v1;
-	std::equal(v1.begin(), v1.begin(), v1.begin()) << std::endl;
-}
+
+	// iterator_traits ---------------------------------------------------------------------------------
+	
+	// -------------------------------------------------------------------------------------------------
+
+} // ft
+
+# endif

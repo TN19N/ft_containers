9cc03b383fb7a8e499ab8298f6e044ea7cf93896
Mustapha-Nawawi-T <85850362+Mustapha-Nawawi-T@users.noreply.github.com>
2022-07-15 18:38:49 +0100

    ++

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: mannouao <mannouao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 17:39:06 by mannouao          #+#    #+#             */
-/*   Updated: 2022/07/15 17:16:14 by mannouao         ###   ########.fr       */
+/*   Updated: 2022/07/15 18:36:21 by mannouao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,19 @@
 
 struct A
 {
+	A(const A& a) {(void)a; std::cout << "A(A& a)" << std::endl; }
 	A()  { std::cout << "A()" << std::endl; };
 	~A() { std::cout << "~A()" << std::endl; }
-
 };
 std::ostream& operator << (std::ostream& _cout , const A& a) { (void)a; _cout << "_:)_"; return(_cout); }
 
-
 int main()
 {
-	ft::vector<A> v(3);
-	std::cout << " -------------------------- " << std::endl;
+	ft::vector<A> a(2, A());
+	
+	std::cout << " ------------------------- " << std::endl;
+	
+	a.reserve(10);
+
 	std::cout << " -------------------------- " << std::endl;
 }

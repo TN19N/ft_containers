0fac4b7d157307eddaf0e56c7d152fd6a865c48c
Mustapha-Nawawi-T <85850362+Mustapha-Nawawi-T@users.noreply.github.com>
2022-07-16 16:30:45 +0100

    ++

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: mannouao <mannouao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 17:39:06 by mannouao          #+#    #+#             */
-/*   Updated: 2022/07/16 11:23:57 by mannouao         ###   ########.fr       */
+/*   Updated: 2022/07/16 16:28:43 by mannouao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,4 @@
 
 int main()
 {
-
-	ft::vector<int> v;
-
-	v.push_back(1337);
-	v.push_back(1337);
-
-	v.insert(v.begin() + 1, 0, 42);
-
-	std::cout << "size : " << v.size() << ", cap : " << v.capacity() << std::endl;
-	for (size_t i = 0; i < v.size(); ++i)
-		std::cout << "=> " << v[i] << std::endl;
 }

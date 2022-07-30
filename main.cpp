e739cd469e2f8448e85ad9e8d839d02af967473b
Mustapha-Nawawi-T <85850362+Mustapha-Nawawi-T@users.noreply.github.com>
2022-07-30 12:25:30 +0100

    ++

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: mannouao <mannouao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 17:39:06 by mannouao          #+#    #+#             */
-/*   Updated: 2022/07/29 17:05:27 by mannouao         ###   ########.fr       */
+/*   Updated: 2022/07/30 12:04:31 by mannouao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,23 +40,8 @@
 #include <memory>
 #include <chrono>
 
-void ft_main()
-{
-	ft::map<int, int> m;
-
-	m[0] = 0;
-	m[1] = 100;
-	m[2] = 200;
-
-	m.erase(m.find(2), m.end());
-
-	for (ft::map<int, int>::const_iterator iter = m.begin(); iter != m.end(); ++iter)
-		std::cout << "[" << iter->first << "] => " << iter->second << std::endl;
-}
+#include <limits.h>
 
 int main()
 {
-	ft_main();
-	system("leaks run");
-	return (0);
 }
